958615ce4b39c39038f4472f57a45c97206ab2d9
btbell <btbell@gmail.com>
2019-02-04 10:54:56 -0500

    created withFunctionComponent iteration of example script as well as created a new example using parent-child component

--- src/index.js
+++ src/index.js
@@ -1,44 +1,33 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 
-let skiData = {
-	total: 50,
-	powder: 20,
-	backcountry: 10,
-	goal: 100
-}
-
-const getPercent = decimal => {
-	return decimal * 100 + '%'
-}
-const calcGoalProgress = (total, goal) => {
-	return getPercent(total/goal)
-}
-
-const SkiDayCounter = ({total, powder, backcountry, goal}) => {
+const Book = ({title, author, pages}) => {
 	return (
 		<section>
-			<h3>Ski Days</h3>
-			<div>
-				<p>Total Days: {total}</p>
-			</div>
-			<div>
-				<p>Powder Days: {powder}</p>
-			</div>
-			<div>
-				<p>Backcountry Days: {backcountry}</p>
-			</div>
-			<div>
-				<p>Goal Progress: {calcGoalProgress(total, goal)}</p>
-			</div>
+			<h2>{title}</h2>
+			<p>by: {author}</p>
+			<p>Pages: {pages} pgs.</p>
 		</section>
 	)
 }
 
+const Library = () => {
+	return (
+		<div>
+			<Book
+				title = "The Sun Also Rises" author ="Ernest Hemmingway"
+				pages = {260} />
+			<Book
+				title = "White Teeth" author ="Zadie Smith"
+				pages = {480} />
+			<Book
+				title = "Cats Cradle" author ="Kurt Vonnegut"
+				pages = {304} />
+		</div>
+	)
+}
+
 render(
-	<SkiDayCounter
-		total={skiData.total}
-		powder={skiData.powder}
-		backcountry={skiData.backcountry}
-		goal={skiData.goal} />, 
-	document.getElementById('root'))
+	<Library />,
+	document.getElementById('root')
+)
